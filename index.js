11b4eb7b8e415b948fb1eef04faab21c8cead82f
etherio <git@myan.app>
2020-12-19 10:56:40 +0630

    cors to static access

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
 const app = express();
+const STATIC_PATH = path.resolve("tmp");
 
 const PORT = process.env.PORT || 8000;
 const middleware = require("./src/middleware");
 const router = require("./router");
 
-app.use(express.static(path.resolve("tmp")));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -24,8 +24,12 @@ app.get("/", (req, res) => {
 app.use(router);
 
 app.use((req, res) => {
-  res.status(404);
-  res.end();
+  const staticPath = path.join(STATIC_PATH, req.path);
+  if (!fs.existsSync(staticPath)) {
+    res.status(404);
+    return res.end();
+  }
+  res.sendFile(staticPath);
 });
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
